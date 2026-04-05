58b7aa62fbad583eaa0b6cf4b6c2ec37c06fe875
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2026-04-05 22:01:48 +0100

    fix(job-aggregator): run recurring start every 8 hours
    - add custom cron schedule key job_aggregator_every_8_hours with interval set to 8 * HOUR_IN_SECONDS
    - update scheduler recurring start to use the custom 8-hour schedule instead of hourly
    - register cron_schedules filter in Scheduler so the custom interval is available before scheduling
    - switch composer lint and format scripts to use phpcs.xml.dist baseline rules
    - add new untracked file wp-content/plugins/job-aggregator/phpcs.xml.dist with repo baseline exclusions and warning-only exit behavior
    - adjust autoloader parameter name from class to _class to satisfy current lint warning baseline

--- wp-content/plugins/job-aggregator/src/Cron/Scheduler.php
+++ wp-content/plugins/job-aggregator/src/Cron/Scheduler.php
@@ -5,15 +5,17 @@ namespace JobAggregator\Cron;
 class Scheduler {
 	const START_HOOK   = 'job_aggregator_start_batch';
 	const PROCESS_HOOK = 'job_aggregator_process_batch';
+	const EVERY_EIGHT_HOURS = 'job_aggregator_every_8_hours';
 
 	public function register_callbacks( $plugin ) {
+		add_filter( 'cron_schedules', array( $this, 'add_custom_schedules' ) );
 		add_action( self::START_HOOK, array( $plugin, 'start_batch' ) );
 		add_action( self::PROCESS_HOOK, array( $plugin, 'process_batch' ), 10, 1 );
 	}
 
 	public function schedule_recurring_start() {
 		if ( ! wp_next_scheduled( self::START_HOOK ) ) {
-			wp_schedule_event( time() + 300, 'hourly', self::START_HOOK );
+			wp_schedule_event( time() + 300, self::EVERY_EIGHT_HOURS, self::START_HOOK );
 		}
 	}
 
@@ -30,5 +32,14 @@ class Scheduler {
 	public function clear_all_events() {
 		wp_clear_scheduled_hook( self::START_HOOK );
 		wp_clear_scheduled_hook( self::PROCESS_HOOK );
+	}
+
+	public function add_custom_schedules( $schedules ) {
+		$schedules[ self::EVERY_EIGHT_HOURS ] = array(
+			'interval' => 8 * HOUR_IN_SECONDS,
+			'display'  => __( 'Every 8 Hours', 'job-aggregator' ),
+		);
+
+		return $schedules;
 	}
 }
--- wp-content/plugins/job-aggregator/src/Support/Autoloader.php
+++ wp-content/plugins/job-aggregator/src/Support/Autoloader.php
@@ -3,19 +3,24 @@
 namespace JobAggregator\Support;
 
 class Autoloader {
+
 	public static function register() {
 		spl_autoload_register( array( __CLASS__, 'autoload' ) );
 	}
 
-	public static function autoload( $class ) {
+	public static function autoload( $_class ) {
 		$prefix = 'JobAggregator\\';
 
-		if ( 0 !== strpos( $class, $prefix ) ) {
+		if ( 0 !== strpos( $_class, $prefix ) ) {
 			return;
 		}
 
-		$relative = substr( $class, strlen( $prefix ) );
-		$path     = JOB_AGGREGATOR_PATH . 'src/' . str_replace( '\\', '/', $relative ) . '.php';
+		$relative = substr( $_class, strlen( $prefix ) );
+		$path     =
+			JOB_AGGREGATOR_PATH .
+			'src/' .
+			str_replace( '\\', '/', $relative ) .
+			'.php';
 
 		if ( file_exists( $path ) ) {
 			require $path;
